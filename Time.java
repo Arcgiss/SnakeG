a70d9a44059a7cc57dd12394520f69b0f3db16a1
Arcgiss <58184891+Arcgiss@users.noreply.github.com>
2020-01-31 09:02:39 +0200

    Time.java

---
+++ Time.java
@@ -0,0 +1,7 @@
+package as;
+
+public class Time {
+    public static double timeStarted = System.nanoTime();
+
+    public static double getTime() { return (System.nanoTime() - timeStarted) * 1E-9; }
+}

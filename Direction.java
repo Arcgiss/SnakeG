27539fb7ab1cc8a3ee838a6360e6e2d3996624d7
Arcgiss <58184891+Arcgiss@users.noreply.github.com>
2020-01-31 08:59:41 +0200

    Direction.java

---
+++ Direction.java
@@ -0,0 +1,22 @@
+public enum Direction {
+    RIGHT,
+    LEFT,
+    UP,
+    DOWN;
+    
+    public static Direction getRIGHT() {
+        return RIGHT;
+    }
+
+    public static Direction getLEFT() {
+        return LEFT;
+    }
+
+    public static Direction getUP() {
+        return UP;
+    }
+
+    public static Direction getDOWN() {
+        return DOWN;
+    }
+}

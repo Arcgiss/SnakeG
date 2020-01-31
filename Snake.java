bf1d4ae708a3ae2c215b404d87e119471ec9a04e
Arcgiss <58184891+Arcgiss@users.noreply.github.com>
2020-01-31 09:18:29 +0200

    Create Snake.java

---
+++ Snake.java
@@ -0,0 +1,73 @@
+package as;
+
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+import javafx.geometry.Point2D;
+
+
+public class Snake {
+    public Rect[] body = new Rect[100];
+    public double bodyWidth, bodyHeight;
+    public int size;
+    public int tail = 0;
+    public int head = 0;
+    public Direction direction = Direction.RIGHT;
+    public double ogWaitBetweenUpdates = 0.2f;
+    public double waitTimeLeft = ogWaitBetweenUpdates;
+    public Rect background;
+     public boolean isDead=false;
+
+
+    public Snake(int size, double startX, double startY, double bodyWidth, double bodyHeight, Rect background) {
+        this.size = size;
+        this.bodyWidth = bodyWidth;
+        this.bodyHeight = bodyHeight;
+        this.background=background;
+
+        for (int i=0; i <= size; i++) {
+            Rect bodyPiece = new Rect(startX + i * bodyWidth, startY, bodyWidth, bodyHeight);
+            body[i] = bodyPiece;
+            head++;
+        }
+        head--;
+    }
+
+   
+    public void changeDirecton(Direction newDirection) {
+        if (newDirection == Direction.RIGHT && direction != Direction.LEFT)
+            direction = newDirection;
+        else if (newDirection == Direction.LEFT && direction != Direction.RIGHT)
+            direction = newDirection;
+        else if (newDirection == Direction.UP && direction != Direction.DOWN)
+            direction = newDirection;
+        else if (newDirection == Direction.DOWN && direction != Direction.UP)
+            direction = newDirection;
+    }
+    public void setDirection(Direction direction) {
+        this.direction=direction;
+    }
+
+    public void getPosition() {
+    }
+
+    public boolean isDead() {
+       return true;
+    }
+    public void draw(Graphics2D g2) {
+        for (int i = tail; i != head; i = (i + 1) % body.length) {
+            Rect piece = body[i];
+            double subWidth = (piece.width - 6.0) / 2.0;
+            double subHeight = (piece.height - 6.0) / 2.0;
+
+            g2.setColor(Color.BLACK);
+            g2.fill(new Rectangle2D.Double(piece.x + 2.0, piece.y + 2.0, subWidth, subHeight));
+            g2.fill(new Rectangle2D.Double(piece.x + 4.0 + subWidth, piece.y + 2.0, subWidth, subHeight));
+            g2.fill(new Rectangle2D.Double(piece.x + 2.0, piece.y + 4.0 + subHeight, subWidth, subHeight));
+            g2.fill(new Rectangle2D.Double(piece.x + 4.0 + subWidth, piece.y + 4.0 + subHeight, subWidth, subHeight));
+        }
+    }
+
+ 
+    
+
+}
